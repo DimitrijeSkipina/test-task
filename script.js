ab94b3e7b1c95adba311404508771946dfe23e3d
Dimitrije <skipina.dimitrije@gmail.com>
2023-06-24 19:06:32 +0200

    fix style gap,font-size,media, fix script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var dataArray = [
+const dataArray = [
     { name: "Отправить на стоянку", price: 400, type: "buying" },
     { name: "Назначить семейным", price: 2200, type: "premium" },
     { name: "Отремонтировать", price: 2200, type: "buying" },
@@ -14,14 +14,14 @@ var dataArray = [
   
 
 
-    var contentBlock = document.querySelector('.content');
+    const contentBlock = document.querySelector('.content');
   
     contentBlock.addEventListener('wheel', function(event) {
-        var scrollAmount = event.deltaY;
+        const scrollAmount = event.deltaY;
         contentBlock.scrollLeft += scrollAmount;
       });
 
-    var items = dataArray.length; // Предполагается, что у вас есть массив данных dataArray
+    const items = dataArray.length;
 
     if (items > 8) {
         contentBlock.classList.add('more-than-eight');
@@ -32,15 +32,15 @@ var dataArray = [
     }
 
   dataArray.forEach(function(item) {
-    var block = document.createElement('div');
+    const block = document.createElement('div');
     block.classList.add('content__data-block');
   
-    var name = document.createElement('div');
+    const name = document.createElement('div');
     name.textContent = item.name;
     name.classList.add('content__data-block-name');
     block.appendChild(name);
   
-    var price = document.createElement('div');
+    const price = document.createElement('div');
     if (item.type === "buying" ) {
         price.innerHTML = "<span class='content__data-block--dollar-sign'>$</span>" + item.price;
         price.classList.add('content__data-block-price');
@@ -56,7 +56,7 @@ var dataArray = [
     }
 
     block.addEventListener('click', function() {
-        console.log(item.name); // Вывод имени элемента в консоль
+        console.log(item.name);
         block.classList.add('content__data-block-dark');
     });
     contentBlock.appendChild(block);
